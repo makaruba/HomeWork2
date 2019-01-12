dead7be68027b751bf776a0d26eea6360eb420f9
Vitaliy <makaruba@mail.ru>
2019-01-13 01:27:02 +0700

    ololo commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        for (String i:args)
+        {
+            System.out.println("Привет, " + i + "!");
+            System.out.println("ololo");
+        }
+
+    }
+}
