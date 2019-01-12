af6ce8a3be2d5f2403cc1cdd483fde0c36aa965c
Vitaliy <makaruba@mail.ru>
2019-01-13 02:23:53 +0700

    HotFix for NewBranch1 commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,17 @@ public class Main {
 
     public static void main(String[] args){
 
-        for (String i:args)
-        {
+        for (String i:args) {
             System.out.println("Привет, " + i + "!");
-            System.out.println("ololo");
-            System.out.println("ololo");
-            System.out.println("NewBranch1");
-            
         }
+        System.out.println("ololo");
+        System.out.println("ololo");
+        System.out.println("NewBranch1");
+        NewClass xxx = new NewClass();
+
+        xxx.doNothing();
+
+
 
     }
 }
---
+++ src/NewClass.java
@@ -0,0 +1,6 @@
+public class NewClass {
+
+    public void doNothing(){
+        System.out.println("Do nothing");
+    }
+}
